7e77b34171e57ebafb3cfc754aa9ba7f9888e2b7
meemkoo <guy99992@gmail.com>
2023-11-05 15:13:32 -0600

    Added testing framework, started VS Code things, added main script

---
+++ src/__main__.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+def main(args):
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main(argv)
---
+++ tests/test_things.py
@@ -0,0 +1,5 @@
+def test_thing_one():
+    assert "Yippie" == "Yippie"
+
+def test_thing_two():
+    assert 10 != 0
